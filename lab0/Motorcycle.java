bf4964f84fa1f3331146be3399bb8731a3db2532
Nate Titterton <natetitterton@gmail.com>
2015-01-21 01:26:03 -0800

    removing indents

--- lab0/Motorcycle.java
+++ lab0/Motorcycle.java
@@ -2,26 +2,26 @@
 
 public class Motorcycle {
 
-  	String make;
-  	String color;
-  	boolean engineState;
+String make;
+String color;
+boolean engineState;
 
-  	void startEngine() {
-    	if (engineState == true) {
-			System.out.println("The engine is already on.");
-		} else {
-			engineState = true;
-         	System.out.println("The engine is now on.");
-     	}
-  	}
+void startEngine() {
+if (engineState == true) {
+System.out.println("The engine is already on.");
+} else {
+engineState = true;
+System.out.println("The engine is now on.");
+}
+}
   
-  	void showAtts() {
-    	System.out.println("This motorcycle is a " + color + " " + make);
-   		if (engineState == true) {
-        	System.out.println("The engine is on.");
-		}
-    		else {
+void showAtts() {
+System.out.println("This motorcycle is a " + color + " " + make);
+if (engineState == true) {
+System.out.println("The engine is on.");
+}
+else {
 System.out.println("The engine is off.");
-  		}
-	}
+}
+}
 }
